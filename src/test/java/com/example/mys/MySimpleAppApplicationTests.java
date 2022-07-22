fbc92b956413408a388f046d62bacc13037aed8b
aryadhigana.s <aryadhigana.s@gmail.com>
2022-07-22 21:57:33 +0700

    First

---
+++ src/test/java/com/example/mys/MySimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
