159082ce5867cb4d4ac89604b25b57ae08044bce
aryadhigana-s <73999466+aryadhigana-s@users.noreply.github.com>
2023-01-13 14:17:09 +0700

    #TAO-11 TAO-11 deactivate function
    #TAO-11 TAO-11 deactivate function

--- src/main/java/com/example/mys/MySimpleAppApplication.java
+++ src/main/java/com/example/mys/MySimpleAppApplication.java
@@ -14,10 +14,10 @@ public class MySimpleAppApplication {
 		return "<h1> Hello World 1 </h1>";
 	}
 	
-	@RequestMapping(value="/hello2")
-	public String coba2() {
-		return "<h1> Hello World </h1>";
-	}
+// 	@RequestMapping(value="/hello2")
+// 	public String coba2() {
+// 		return "<h1> Hello World </h1>";
+// 	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MySimpleAppApplication.class, args);
